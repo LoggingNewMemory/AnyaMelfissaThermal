bf469c13e986155c2dd88425fcec60a4e8b30e11
LoggingNewMemory <albert.wesley.dion@gmail.com>
2025-09-17 22:51:38 +0700

    initial action.sh redesign banner redesign logo

---
+++ Modules/action.sh
@@ -0,0 +1,40 @@
+#!/system/bin/sh
+#
+# This script allows enabling or disabling thermal management
+# using the volume keys by executing specific scripts.
+
+# A function to print messages to the UI.
+ui_print() {
+  echo "$1"
+}
+
+ui_print " "
+ui_print "Anya Control Panel"
+ui_print " "
+ui_print "Please use the volume keys to make your selection."
+ui_print " "
+ui_print "Volume UP   = Disable Thermal"
+ui_print "Volume DOWN = Enable Thermal"
+ui_print " "
+
+while true; do
+  EVENT=$(getevent -lqc 1)
+
+  if echo "$EVENT" | grep -q "KEY_VOLUMEUP.*DOWN"; then
+    /data/adb/modules/AnyaMelfissa/AnyaMelfissa/AnyaMelfissa.sh
+    ui_print "Anya Melfissa!"
+    break 
+
+  elif echo "$EVENT" | grep -q "KEY_VOLUMEDOWN.*DOWN"; then
+    ui_print "Enabling Thermal..."
+    /data/adb/modules/AnyaMelfissa/AnyaMelfissa/AnyaKawaii.sh
+    ui_print "Anya Kawaii!"
+    break 
+  fi
+done
+
+ui_print " "
+ui_print "I love Anya Melfissa"
+ui_print " "
+
+exit 0
