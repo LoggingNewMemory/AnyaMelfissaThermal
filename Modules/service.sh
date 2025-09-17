dc57c62aa5b9385b069a262cfe5ea5dbad53ca79
LoggingNewMemory <albert.wesley.dion@gmail.com>
2025-09-17 20:45:26 +0700

    Core only

--- Modules/service.sh
+++ Modules/service.sh
@@ -2,16 +2,4 @@ while [ -z "$(getprop sys.boot_completed)" ]; do
 sleep 10
 done
 
-tweak() {
-	if [ -f "$2" ]; then
-		chmod 644 "$2" >/dev/null 2>&1
-		echo "$1" >"$2" 2>/dev/null
-		chmod 444 "$2" >/dev/null 2>&1
-	fi
-}
-
-for trip in /sys/class/thermal/thermal_zone*/trip_point*; do
-    tweak 999999999 "$trip"
-done
-
 sh /data/adb/modules/AnyaMelfissa/AnyaMelfissa/AnyaMelfissa.sh
