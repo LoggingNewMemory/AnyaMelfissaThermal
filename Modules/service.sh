97c374fa9f8d02fdf2121809d1b794060ace5c92
LoggingNewMemory <albert.wesley.dion@gmail.com>
2025-09-17 09:00:37 +0700

    modify just temp inside thermal zone

--- Modules/service.sh
+++ Modules/service.sh
@@ -14,4 +14,6 @@ for trip in /sys/class/thermal/thermal_zone*/trip_point*; do
     tweak 999999999 "$trip"
 done
 
+find /sys/devices/virtual/thermal/thermal_zone*/temp -type f -exec sh -c 'echo "0" > "$1" && chmod 444 "$1"' _ {} \;
+
 sh /data/adb/modules/AnyaMelfissa/AnyaMelfissa/AnyaMelfissa.sh
