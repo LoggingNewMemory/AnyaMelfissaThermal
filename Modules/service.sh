5523cb48ecbdade7b2e228380b4e6cce06be0259
KanagawaYamada <albert.wesley.dion@gmail.com>
2026-08-15 20:13:32 +0700

    Gini lebih cocok
    Signed-off-by: KanagawaYamada <albert.wesley.dion@gmail.com>

--- Modules/service.sh
+++ Modules/service.sh
@@ -5,5 +5,5 @@ done
 START_ON_BOOT=$(cat /data/adb/modules/AnyaMelfissa/StartOnBoot.txt)
 if [ "$START_ON_BOOT" = "1" ]; then
   /data/adb/modules/AnyaMelfissa/AnyaMelfissa/AnyaMelfissa 1
-  su -lp 2000 -c "cmd notification post -S bigtext -t 'Anya Melfissa' -i file:///data/local/tmp/Anya.png -I file:///data/local/tmp/Anya.png TagAnya 'Good Day! Thermal Is Dead BTW'"
+  su -lp 2000 -c "cmd notification post -S bigtext -t 'Anya Melfissa' -i file:///data/local/tmp/Anya.png -I file:///data/local/tmp/Anya.png TagAnya 'Good Day! Thermal modification is done. Now let me sleep'"
 fi
