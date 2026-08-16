ac98b7b68690b0d67f5440b671c791c8dbaa67d8
KanagawaYamada <albert.wesley.dion@gmail.com>
2026-08-16 20:20:22 +0700

    Added fake trip for later usage
    Signed-off-by: KanagawaYamada <albert.wesley.dion@gmail.com>

--- Sources/AnyaMelfissa.c
+++ Sources/AnyaMelfissa.c
@@ -12,15 +12,24 @@ Copyright (C) 2026 Kanagawa Yamada
 #include <sys/mount.h>
 #include <sys/stat.h>
 
-
 #define FAKE_TEMP_FILE "/data/local/tmp/fake_temp"
+#define FAKE_TRIP_FILE "/data/local/tmp/fake_trip"
 #define FAKE_TEMP_VALUE "30000\n"
+#define FAKE_TRIP_VALUE "200000\n"
 #define THERMAL_DIR "/sys/class/thermal"
 
 void create_fake_temp_file() {
     int fd = open(FAKE_TEMP_FILE, O_CREAT | O_WRONLY | O_TRUNC, 0644);
     if (fd >= 0) {
         write(fd, FAKE_TEMP_VALUE, strlen(FAKE_TEMP_VALUE));
+        close(fd);
+    }
+}
+
+void create_fake_trip_file() {
+    int fd = open(FAKE_TRIP_FILE, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+    if (fd >= 0) {
+        write(fd, FAKE_TRIP_VALUE, strlen(FAKE_TRIP_VALUE));
         close(fd);
     }
 }
@@ -47,6 +56,28 @@ void spoof_temperatures() {
     closedir(dir);
 }
 
+void spoof_trip() {
+    create_fake_trip_file();
+
+    DIR *dir = opendir(THERMAL_DIR);
+    if (!dir) return;
+
+    struct dirent *ent;
+    while ((ent = readdir(dir)) != NULL) {
+        if (strncmp(ent->d_name, "thermal_zone", 12) == 0) {
+            char trip_path[512];
+            snprintf(trip_path, sizeof(trip_path), "%s/%s/trip_point_0_temp", THERMAL_DIR, ent->d_name);
+
+            // Unmount first in case it's already mounted to prevent stacking
+            umount2(trip_path, MNT_DETACH);
+
+            // Bind fake trip point file
+            mount(FAKE_TRIP_FILE, trip_path, NULL, MS_BIND, NULL);
+        }
+    }
+    closedir(dir);
+}
+
 void restore_temperatures() {
     DIR *dir = opendir(THERMAL_DIR);
     if (!dir) return;
@@ -66,12 +97,32 @@ void restore_temperatures() {
     unlink(FAKE_TEMP_FILE);
 }
 
+void restore_trip() {
+    DIR *dir = opendir(THERMAL_DIR);
+    if (!dir) return;
+
+    struct dirent *ent;
+    while ((ent = readdir(dir)) != NULL) {
+        if (strncmp(ent->d_name, "thermal_zone", 12) == 0) {
+            char trip_path[512];
+            snprintf(trip_path, sizeof(trip_path), "%s/%s/trip_point_0_temp", THERMAL_DIR, ent->d_name);
+
+            umount2(trip_path, MNT_DETACH);
+        }
+    }
+    closedir(dir);
+    
+    unlink(FAKE_TRIP_FILE);
+}
+
 void exec_anya_kawaii() {
     restore_temperatures();
+    restore_trip();
 }
 
 void exec_anya_melfissa() {
     spoof_temperatures();
+    spoof_trip();
 }
 
 int main(int argc, char *argv[]) {
