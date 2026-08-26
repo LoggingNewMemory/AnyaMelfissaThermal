2720223e60b2fef9a6873495380e0675850b0b50
KanagawaYamada <albert.wesley.dion@gmail.com>
2026-08-26 17:55:19 +0700

    Wipe anya state to 0 on service
    Signed-off-by: KanagawaYamada <albert.wesley.dion@gmail.com>

--- Modules/service.sh
+++ Modules/service.sh
@@ -2,7 +2,9 @@ while [ -z "$(getprop sys.boot_completed)" ]; do
 sleep 10
 done
 
-START_ON_BOOT=$(grep "^START_ON_BOOT=" /data/adb/modules/AnyaMelfissa/AnyaConfig.txt | cut -d= -f2)
+sed -i 's/^ANYA .*/ANYA 0/' /data/adb/modules/AnyaMelfissa/AnyaConfig.txt
+
+START_ON_BOOT=$(grep "^START_ON_BOOT " /data/adb/modules/AnyaMelfissa/AnyaConfig.txt | awk '{print $2}')
 if [ "$START_ON_BOOT" = "1" ]; then
   sh /data/adb/modules/AnyaMelfissa/AnyaOn.sh
   su -lp 2000 -c "cmd notification post -S bigtext -t 'Anya Melfissa' -i file:///data/local/tmp/Anya.png -I file:///data/local/tmp/Anya.png TagAnya 'Good Day! Thermal modification is done. Now let me sleep'"
