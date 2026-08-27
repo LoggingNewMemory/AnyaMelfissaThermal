31f94c01cbd5649dbe265a022e4407479880fcb9
KanagawaYamada <albert.wesley.dion@gmail.com>
2026-08-27 11:11:15 +0700

    [STABLE] Anya Stable
    Signed-off-by: KanagawaYamada <albert.wesley.dion@gmail.com>

--- Sources/AnyaMelfissa.c
+++ Sources/AnyaMelfissa.c
@@ -13,11 +13,7 @@ Copyright (C) 2026 Kanagawa Yamada
 #include <sys/stat.h>
 
 #define FAKE_TEMP_FILE "/data/adb/modules/AnyaMelfissa/fake_temp"
-#define FAKE_TRIP_FILE "/data/adb/modules/AnyaMelfissa/fake_trip"
-#define FAKE_ZERO_FILE "/data/adb/modules/AnyaMelfissa/fake_zero"
 #define FAKE_TEMP_VALUE "30000\n"
-#define FAKE_TRIP_VALUE "200000\n"
-#define FAKE_ZERO_VALUE "0\n"
 #define THERMAL_DIR "/sys/class/thermal"
 
 FILE *sh_pipe = NULL;
@@ -42,22 +38,6 @@ void create_fake_temp_file() {
     }
 }
 
-void create_fake_trip_file() {
-    int fd = open(FAKE_TRIP_FILE, O_CREAT | O_WRONLY | O_TRUNC, 0644);
-    if (fd >= 0) {
-        write(fd, FAKE_TRIP_VALUE, strlen(FAKE_TRIP_VALUE));
-        close(fd);
-    }
-}
-
-void create_fake_zero_file() {
-    int fd = open(FAKE_ZERO_FILE, O_CREAT | O_WRONLY | O_TRUNC, 0644);
-    if (fd >= 0) {
-        write(fd, FAKE_ZERO_VALUE, strlen(FAKE_ZERO_VALUE));
-        close(fd);
-    }
-}
-
 void write_sysfs(const char *path, const char *val) {
     int fd = open(path, O_WRONLY);
     if (fd >= 0) {
@@ -68,8 +48,6 @@ void write_sysfs(const char *path, const char *val) {
 
 void spoof_thermal() {
     create_fake_temp_file();
-    create_fake_trip_file();
-    create_fake_zero_file();
 
     sh_pipe = popen("sh", "w");
     if (!sh_pipe) return;
@@ -100,47 +78,7 @@ void spoof_thermal() {
             umount_all(temp_path);
             // Bind mount the fake files over the real ones
             mount_bind(FAKE_TEMP_FILE, temp_path);
-            
-            DIR *zdir = opendir(zone_path);
-            if (zdir) {
-                struct dirent *zent;
-                while ((zent = readdir(zdir)) != NULL) {
-                    if (strncmp(zent->d_name, "trip_point_", 11) == 0 && strstr(zent->d_name, "_temp") != NULL) {
-                        char trip_path[512];
-                        snprintf(trip_path, sizeof(trip_path), "%s/%s", zone_path, zent->d_name);
-                        umount_all(trip_path);
-                        mount_bind(FAKE_TRIP_FILE, trip_path);
-                    } else if (strncmp(zent->d_name, "trip_point_", 11) == 0 && strstr(zent->d_name, "_hyst") != NULL) {
-                        char hyst_path[512];
-                        snprintf(hyst_path, sizeof(hyst_path), "%s/%s", zone_path, zent->d_name);
-                        umount_all(hyst_path);
-                        mount_bind(FAKE_ZERO_FILE, hyst_path);
-                    } else if (strncmp(zent->d_name, "cdev", 4) == 0 && strstr(zent->d_name, "_trip_point") != NULL) {
-                        char cdev_path[512];
-                        snprintf(cdev_path, sizeof(cdev_path), "%s/%s", zone_path, zent->d_name);
-                        umount_all(cdev_path);
-                        mount_bind(FAKE_ZERO_FILE, cdev_path);
-                    } else if (strncmp(zent->d_name, "cdev", 4) == 0 && strstr(zent->d_name, "_weight") != NULL) {
-                        char weight_path[512];
-                        snprintf(weight_path, sizeof(weight_path), "%s/%s", zone_path, zent->d_name);
-                        umount_all(weight_path);
-                        mount_bind(FAKE_ZERO_FILE, weight_path);
-                    }
-                }
-                closedir(zdir);
-            }
-        } else if (strncmp(ent->d_name, "cooling_device", 14) == 0) {
-            char cdev_dir[512];
-            snprintf(cdev_dir, sizeof(cdev_dir), "%s/%s", THERMAL_DIR, ent->d_name);
-            
-            char cur_state_path[512];
-            snprintf(cur_state_path, sizeof(cur_state_path), "%s/cur_state", cdev_dir);
-            
-            umount_all(cur_state_path);
-            
-            mount_bind(FAKE_ZERO_FILE, cur_state_path);
         }
-        
     }
     closedir(dir);
     if (sh_pipe) {
@@ -177,41 +115,7 @@ void restore_thermal() {
 
             // Unmount the fake files with MNT_DETACH to prevent EBUSY
             umount_all(temp_path);
-            
-            DIR *zdir = opendir(zone_path);
-            if (zdir) {
-                struct dirent *zent;
-                while ((zent = readdir(zdir)) != NULL) {
-                    if (strncmp(zent->d_name, "trip_point_", 11) == 0 && strstr(zent->d_name, "_temp") != NULL) {
-                        char trip_path[512];
-                        snprintf(trip_path, sizeof(trip_path), "%s/%s", zone_path, zent->d_name);
-                        umount_all(trip_path);
-                    } else if (strncmp(zent->d_name, "trip_point_", 11) == 0 && strstr(zent->d_name, "_hyst") != NULL) {
-                        char hyst_path[512];
-                        snprintf(hyst_path, sizeof(hyst_path), "%s/%s", zone_path, zent->d_name);
-                        umount_all(hyst_path);
-                    } else if (strncmp(zent->d_name, "cdev", 4) == 0 && strstr(zent->d_name, "_trip_point") != NULL) {
-                        char cdev_path[512];
-                        snprintf(cdev_path, sizeof(cdev_path), "%s/%s", zone_path, zent->d_name);
-                        umount_all(cdev_path);
-                    } else if (strncmp(zent->d_name, "cdev", 4) == 0 && strstr(zent->d_name, "_weight") != NULL) {
-                        char weight_path[512];
-                        snprintf(weight_path, sizeof(weight_path), "%s/%s", zone_path, zent->d_name);
-                        umount_all(weight_path);
-                    }
-                }
-                closedir(zdir);
-            }
-        } else if (strncmp(ent->d_name, "cooling_device", 14) == 0) {
-            char cdev_dir[512];
-            snprintf(cdev_dir, sizeof(cdev_dir), "%s/%s", THERMAL_DIR, ent->d_name);
-            
-            char cur_state_path[512];
-            snprintf(cur_state_path, sizeof(cur_state_path), "%s/cur_state", cdev_dir);
-            
-            umount_all(cur_state_path);
         }
-        
     }
     closedir(dir);
     if (sh_pipe) {
@@ -220,8 +124,6 @@ void restore_thermal() {
     }
     
     unlink(FAKE_TEMP_FILE);
-    unlink(FAKE_TRIP_FILE);
-    unlink(FAKE_ZERO_FILE);
 }
 
 void exec_anya_kawaii() {
